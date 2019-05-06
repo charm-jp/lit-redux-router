04884376b7bd1519918097bc43fa1f62e2301104
Fernando <fernando@pasik.com.ar>
2019-05-06 02:24:04 +0100

    docs: use material typography

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,26 @@
-import { LitElement, html } from 'https://unpkg.com/lit-element/lit-element.js?module';
+import { LitElement, html, css } from 'https://unpkg.com/lit-element/lit-element.js?module';
 
 class AppDocs extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        font-size: 16px;
+        line-height: 1.5;
+        color: #111;
+        background-color: #ECEFF1;
+        --mdc-theme-primary: #2196f3;
+      }
+    `;
+  }
+
   render() {
-    return html`<div class="app"></div>`;
+    return html`
+      <style>
+        @import url(https://unpkg.com/@material/typography/dist/mdc.typography.min.css);
+      </style>
+      <div class="app mdc-typography">
+      </div>
+    `;
   }
 }
 
