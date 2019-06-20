bd78ba0b0216ad7055667b539f68017629645c79
Fernando Pasik <fernando@pasik.com.ar>
2019-06-20 21:30:50 +0100

    fix: library import

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,5 @@
 import { LitElement, html, css } from 'https://unpkg.com/lit-element/lit-element.js?module';
-import { connectRouter } from 'https://unpkg.com/lit-redux-router/dist/index.js?module';
-
+import { connectRouter } from 'https://unpkg.com/lit-redux-router/dist/lit-redux-router.js?module';
 import store from './store.js';
 
 connectRouter(store);
