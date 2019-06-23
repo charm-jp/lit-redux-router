07a51c0628f59253c28553fa9526880e34ef53cc
Fernando Pasik <fernando@pasik.com.ar>
2019-06-23 22:23:19 +0100

    feat: content table

--- docs/main.js
+++ docs/main.js
@@ -11,23 +11,58 @@ connectRouter(store);
 class AppMain extends LitElement {
   static get styles() {
     return css`
+      .app-main {
+        max-width: 1260px;
+        margin: 0 auto;
+        position: relative;
+        padding-right: 250px;
+        box-sizing: border-box;
+      }
+
       main {
         min-height: 300px;
         box-sizing: border-box;
         padding: 80px 20px 20px;
-        max-width: 1260px;
-        margin: 0 auto;
+      }
+
+      aside {
+        box-sizing: border-box;
+        padding: 113px 20px 20px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 250px;
+      }
+
+      aside a {
+        color: inherit;
+        text-decoration: none;
+      }
+
+      aside a:hover {
+        text-decoration: underline;
       }
     `;
   }
 
   render() {
     return html`
-      <main>
-        <lit-route component="app-not-found"></lit-route>
-        <lit-route path="/" component="app-home"></lit-route>
-        <lit-route path="/getting-started" component="app-getting-started"></lit-route>
-      </main>
+      <style>
+        @import url(https://unpkg.com/@material/typography/dist/mdc.typography.min.css);
+      </style>
+      <div class="app-main">
+        <main>
+          <lit-route component="app-not-found"></lit-route>
+          <lit-route path="/" component="app-home"></lit-route>
+          <lit-route path="/getting-started" component="app-getting-started"></lit-route>
+        </main>
+        <aside>
+          <nav>
+            <h1 class="mdc-typography--headline5">Content table</h1>
+            <a href="/getting-started">Getting Started</a>
+          </nav>
+        </aside>
+      </div>
     `;
   }
 }
